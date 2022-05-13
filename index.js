03e624177bee4e56435a23e28d8bd505616e35c7
imrannaazir <imrannaaziremon@gmail.com>
2022-05-13 11:43:28 +0600

    get api for services

--- index.js
+++ index.js
@@ -10,10 +10,46 @@ app.use(cors());
 app.use(express.json());
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.njnzf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db("dental").collection("services")
+
+
+        //get all services api
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+    }
+    finally {
+
+    }
+}
+
+/* 
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+  }); */
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello from the back end')
 });
 
 app.listen(port, () => {
     console.log('This is ok!');
-})
+});
+
+run().catch(console.dir)
